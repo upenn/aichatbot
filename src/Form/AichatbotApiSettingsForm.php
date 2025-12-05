96a166c0942415b97fa11a7f8b421c16c6fd88cc
Rick Ward <rickward@upenn.edu>
2025-12-05 11:51:02 -0500

    setting api_key

--- src/Form/AichatbotApiSettingsForm.php
+++ src/Form/AichatbotApiSettingsForm.php
@@ -102,11 +102,11 @@ class AichatbotApiSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $key_name = $form_state->getValue('sendgrid_integration_apikey');
+    $key_name = $form_state->getValue('api_key');
     $this->config('aichatbot.settings')
       ->set('ai_service', $form_state->getValue('ai_service'))
 	    ->set('api_url', trim($form_state->getValue('api_url')))
-      ->set('apikey', $key_name)
+      ->set('api_key', $key_name)
       ->set('model', trim($form_state->getValue('model')))
       ->save();
 
